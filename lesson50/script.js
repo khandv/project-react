7d2f677fbae6599bfe94f5ee20e6861c5b1d6153
Khan <khandv@gmail.com>
2020-04-12 17:52:12 +0300

    hw50-2

--- lesson50/script.js
+++ lesson50/script.js
@@ -3,20 +3,9 @@ let employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'A
 let employersNames = employers.filter((employer) => {
     return employer.length > 0 && employer.length != ''
 });
-
-console.log(employersNames);
-
-// for (let i = 0; i < employers.length; i++) {
-// 	if (employers[i].length > 0 && employers[i].length != '') {
-// 		employersNames.push(employers[i]);
-// 	}
-// }
-
 employersNames = employersNames.map((employer) => employer.toLowerCase().trim());
 
-// for (let i = 0; i < employersNames.length; i++) {
-// 	employersNames[i] = employersNames[i].toLowerCase().trim();
-// }
+console.log(employersNames);
 
 let sponsors = {
     cash: [40000, 5000, 30400, 12000],
@@ -42,9 +31,8 @@ function makeBusiness(owner, director = 'Victor', cash, emp) {
     let sumSponsors = sponsors.eu.concat(sponsors.rus, 'unexpected sponsor');
     // console.log('We have a business. Owner: ' + owner + ', director: ' + director + '. Our budget: ' + cash + '. And our employers: ' +
     // emp);
-    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}`);
-    console.log('And we have a sponsors: ');
-    console.log.apply(null, sumSponsors);
-    console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
+    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp},
+    And we have a sponsors: ${sumSponsors}`);
+    console.log(`Note. Be careful with ${sponsors.eu[0]}. It's a huge risk.`);
 }
 makeBusiness.apply(null, ['Sam', null, money, employersNames]);
